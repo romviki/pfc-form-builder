9869e6079417423ab075e1d88670ffb71c2061cd
Marcus Ho <marcusho1993@gmail.com>
2022-01-28 16:36:00 -0500

    added .prettierrc

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 import './App.css';
 
 function App() {
-	return (
-		<div>
-			<h1>PFC Form Builder</h1>
+  return (
+    <div>
+      <h1>PFC Form Builder</h1>
 
-			<Router>
-				<Routes>
-					<Route path='/' element={<h2>Main Page</h2>} />
-					<Route path='*' element={<Navigate replace to='/' />} />
-				</Routes>
-			</Router>
-		</div>
-	);
+      <Router>
+        <Routes>
+          <Route path='/' element={<h2>Main Page</h2>} />
+          <Route path='*' element={<Navigate replace to='/' />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
