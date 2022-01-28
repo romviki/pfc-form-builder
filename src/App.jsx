7668ee7d69e9b34ae93263ee38c10bac8807587e
Marcus Ho <marcusho1993@gmail.com>
2022-01-28 16:07:16 -0500

    cleaned up init proejct and added react-router-dom

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import './App.css';
+
+function App() {
+	return (
+		<div>
+			<h1>PFC Form Builder</h1>
+
+			<Router>
+				<Routes>
+					<Route path='/' element={<h2>Main Page</h2>} />
+					<Route path='*' element={<Navigate replace to='/' />} />
+				</Routes>
+			</Router>
+		</div>
+	);
+}
+
+export default App;
