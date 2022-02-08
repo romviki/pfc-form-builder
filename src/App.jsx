550b4ae546deb15e4d5e1cc14da3a3f3bab862e1
Marcus Ho <marcusho1993@gmail.com>
2022-02-07 22:47:02 -0500

    Added 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,18 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from 'react-router-dom';
-import './App.css';
-import { TextField, Typography } from '@mui/material';
+import { Container, Typography } from '@mui/material';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Notfound from './pages/Notfound';
 
 function App() {
   return (
-    <div>
-      {/* below 2 components are for testing purposes, remove it once development starts */}
-      <Typography variant='h2'>PFC Form Builder</Typography>
-      <TextField id='outlined-basic' label='Testing' variant='outlined' />
-
+    <Container>
       <Router>
         <Routes>
-          <Route path='/' element={<h2>Main Page</h2>} />
-          <Route path='*' element={<Navigate replace to='/' />} />
+          <Route path='/' element={<Dashboard />} />
+          <Route path='*' element={<Notfound />} />
         </Routes>
       </Router>
-    </div>
+    </Container>
   );
 }
 
---
+++ src/pages/Notfound.jsx
@@ -0,0 +1,39 @@
+import { Grid, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+const Notfound = () => {
+  const navigate = useNavigate();
+  const [countDown, setCountDown] = useState(5);
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigate('/');
+    }, 5000);
+
+    const timer = setInterval(() => {
+      setCountDown(prev => prev - 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  });
+
+  return (
+    <Grid container direction='column' justifyContent='center'>
+      <Typography variant='h2' marginTop={12} textAlign='center'>
+        404 Not Found
+      </Typography>
+      <Typography variant='h4' marginTop={4} textAlign='center'>
+        Redirect in {countDown} seconds
+      </Typography>
+      <Box marginTop={4} textAlign='center'>
+        <Link to='/'>Back to Homepage</Link>
+      </Box>
+    </Grid>
+  );
+};
+
+export default Notfound;
